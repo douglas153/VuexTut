da4495daa0cd2e8b57b9bfa8a79b770a1c70d73c
Douglas Sousa <douglas.sousa@compline.com.br>
2020-02-03 18:01:03 -0300

    first commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,28 @@
+<template>
+    <div >
+        <h3 class="todos">Todos: {{allTodos}}</h3>
+
+        <ul v-for="(todo, i) in allTodos" :key="i">
+            <li>{{todo.title}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'; 
+
+export default {
+    data(){
+        return{
+
+        }
+    },
+    computed: mapGetters(['allTodos'])
+
+    
+}
+</script>
+
+<style scoped>
+
+</style>
